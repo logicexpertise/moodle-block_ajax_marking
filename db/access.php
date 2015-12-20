176953486f9f96f301bcb56e4858608aa250b3c5
Olumuyiwa Taiwo <muyi.taiwo@logicexpertise.com>
2015-12-20 14:01:27 +1100

    - fix block does not define capability block/ajax_marking:addinstance

---
+++ db/access.php
@@ -0,0 +1,54 @@
+<?php
+
+/* 
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ *
+ * @package     block/ajax_marking
+ * @subpackage
+ * @copyright   2015 Olumuyiwa Taiwo
+ * @author      Olumuyiwa Taiwo {@link https://moodle.org/user/view.php?id=416594}
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$capabilities = array(
+
+    'block/ajax_marking:myaddinstance' => array(
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => array(
+            'user' => CAP_ALLOW
+        ),
+
+        'clonepermissionsfrom' => 'moodle/my:manageblocks'
+    ),
+
+    'block/ajax_marking:addinstance' => array(
+        'riskbitmask' => RISK_SPAM | RISK_XSS,
+
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_BLOCK,
+        'archetypes' => array(
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ),
+
+        'clonepermissionsfrom' => 'moodle/site:manageblocks'
+    ),
+);
--- version.php
+++ version.php
@@ -25,8 +25,8 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$plugin->version = 2013021002;
+$plugin->version = 2015122000;
 $plugin->maturity = MATURITY_BETA;
-$plugin->requires = 2012120300; // 2.4.
+$plugin->requires = 2015111600; // 3.0+
 $plugin->component = 'block_ajax_marking';
-$plugin->release = '2.4.2';
+$plugin->release = '3.0.0';
